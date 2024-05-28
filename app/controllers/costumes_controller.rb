f52bf603c0dda520b4ca7ab72d1e6ef701d7d87e
joachim <davainjoachim@hotmail.be>
2024-05-28 15:31:45 +0200

    created costumes controller + wrote all methods

---
+++ app/controllers/costumes_controller.rb
@@ -0,0 +1,53 @@
+class CostumesController < ApplicationController
+  before_action :find_user, only: %i[show edit]
+  before_action :find_costume, only: %i[update destroy show edit]
+
+  def index
+    @costumes = Costume.all
+    @costume = Costume.new
+  end
+
+  def show
+    @booking = Booking.new
+  end
+
+  def create
+    costume = Costume.new(costume_params)
+    costume.user = current_user
+    if costume.save!
+      redirect_to costume_path(costume)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @costume.update(costume_params)
+      redirect_to to costume_path(@costume), notice: 'Costume was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @costume.destroy
+    redirect_to costume_path(@costume), status: :see_other
+  end
+
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  def find_costume
+    @costume = Costume.find(params[:id])
+  end
+
+  def costume_params
+    params.require(:costume).permit(:title, :description, :price_per_day)
+  end
+end
