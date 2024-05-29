ca3800772c63bd4b391dedf4d6b964b63bd066bc
joachim <davainjoachim@hotmail.be>
2024-05-29 18:11:20 +0200

    stfu

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,4 @@ class User < ApplicationRecord
   validates :email, format: { with: Devise.email_regexp }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/ }
   has_one_attached :photo
-
 end
