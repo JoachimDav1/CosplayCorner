21be4d2c9416acf9c6e6ac82a182cdc235843c59
joachim <davainjoachim@hotmail.be>
2024-05-30 11:56:04 +0200

    Updated costume model, updated booking validation, updated seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_122324) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_093035) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_122324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "category"
+    t.string "size"
     t.index ["user_id"], name: "index_costumes_on_user_id"
   end
 
