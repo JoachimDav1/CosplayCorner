40d9d8845853554ad31d6156adb625f118fd6e04
Aude Cordemans <aude.cordemans@gmail.com>
2024-05-31 12:56:11 +0200

    search fix

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,16 +2,11 @@ import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
 export default class extends Controller {
   connect() {
-
     flatpickr(this.element, {
-      inline: true,
-      mode: 'range',
       minDate: "today",
       enableTime: false,
       dateFormat: "Y-m-d",
       mode: "range"
     });
   }
-
-
 }
